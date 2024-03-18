bd55ebaa21c9790d679d4c31e3c698f5be5833e5
Samuel Y.-H. Pan <samuel.yhpan@gmail.com>
2024-03-18 10:37:16 +0800

    Add files via upload

---
+++ data.json.js
@@ -0,0 +1,28 @@
+const data = [
+//data start
+{lx:"acu'",gn:"狗"},{lx:"babu'",gn: "豬"},{lx:"tumaz",gn: "熊"},{lx:"tas'a",gn:"一"},
+{lx:"dusa",gn:"二"},{lx:"tau",gn:"三"},{lx:"pat",gn:"四"},{lx:"hima",gn:"五"},
+{lx:"num",gn:"六"},{lx:"pitu",gn:"七"},{lx:"vau",gn:"八"},{lx:"siva",gn:"九"},
+{lx:"mas'an",gn:"十"},{lx:"mas'an qan tas'a",gn:"十一"},{lx:"mas'an qan dusa",gn:"十二"},
+{lx:"mas'an qan tauu",gn:"十三"},{lx:"mas'an qan pat",gn:"十四"},{lx:"mas'an qan hima",gn:"十五"},
+{lx:"mas'an qan num",gn:"十六"},{lx:"mas'an qan pitu",gn:"十七"},{lx:"mas'an qan vau",gn:"十八"},
+{lx:"mas'an qan siva",gn:"十九"},{lx:"mapusan",gn:"二十"},{lx:"mapusan qan tac-a",gn:"二十一"},
+{lx:"mapusan qan dusa",gn:"二十二"},{lx:"mapusan qan tauu",gn:"二十三"},{lx:"mapusan qan pat",gn:"二十四"},
+{lx:"mapusan qan hima",gn:"二十五"},{lx:"mati'un",gn:"三十"},
+{lx:"masipatun",gn:"四十"},{lx:"mahimaun",gn:"五十"},
+{lx:"manumun",gn:"六十"},{lx:"mapitu-un",gn:"七十"},
+{lx:"mavau-un",gn:"八十"},{lx:"masivaun",gn:"九十"},
+{lx:"zaku",gn:"我"},{lx:"inak",gn:"我的"},
+{lx:"su",gn:"你"},{lx:"isu",gn:"你的"},
+{lx:"cia",gn:"他"},{lx:"icia",gn:"他的"},
+{lx:"mita",gn:"我們"},{lx:"imita",gn:"我們的"},
+{lx:"mu",gn:"你們"},{lx:"imuu",gn:"你們的"},
+{lx:"icia",gn:"他們"},{lx:"iciata",gn:"他們的"},
+{lx:"di",gn:"這個"},{lx:"macka",gn:"那個"},
+{lx:"macki",gn:"這裡"},{lx:"macka",gn:"那裡"},
+{lx:"mita",gn:"咱們"},{lx:"imita",gn:"咱們的"},
+{lx:"i", lv:1, gn:"屬格標記", ge:"[genitive marker]", sd:"36助詞或其他", xv:"Maqa hung di ka inak sinbaliv iscaiv vi noba’.", xn:"那本書是我買來送給妹妹的。"},
+{lx:"idi", lv:1, gn:"在(這裡)", ge:"...is here.", sd:"36助詞或其他", xv:"Idi ka nipicu su.", xn:"你的筆在這裡。"},
+{lx:"pu'acu'",gn:"用獵犬追捕獵物"}
+//data end
+];
